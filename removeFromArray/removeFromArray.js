0f0013b168d28ff15da911eb0a55bc174da9a4dd
blas.rodriguez <blas.rodriguez@edreamsodigeo.com>
2020-01-20 08:02:20 +0100

    completed exercise removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(originalArray, ...elementsToDelete) {
+	var filteredArray = originalArray.filter(function(originalArrayElement) {
+		return elementsToDelete.includes(originalArrayElement) ? null:originalArrayElement 
+		});
+	return (filteredArray) ;
 }
 
 module.exports = removeFromArray
