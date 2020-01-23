9c212117798d6daa418efd709e56bc1f630e4a22
blas.rodriguez <blas.rodriguez@edreamsodigeo.com>
2020-01-23 08:58:39 +0100

    JavaScript Fundamentals Part 4w

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(fromNumber, toNumber) {
+	if (!is_Natural(fromNumber)|!is_Natural(toNumber)) 
+		return 'ERROR'
+	if (toNumber < fromNumber) {
+		[toNumber, fromNumber] = [fromNumber, toNumber];
+	}
+	let finalNumber = 0;
+
+	for (let i = fromNumber; i<= toNumber; i++) {
+		finalNumber += i;
+	}
+	return finalNumber;
 
 }
+
+function is_Natural(n)
+      {
+	   if (typeof n !== 'number')
+	        return false;
+
+	 return (n >= 0.0) && (Math.floor(n) === n) && n !== Infinity;
+	    }
 
 module.exports = sumAll
--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheitTemp) {
+	return round((fahrenheitTemp - 32) * (5 / 9))
 }
 
-const ctof = function() {
+const ctof = function(celsisusTemp) {
+	return round(celsisusTemp * (9 / 5) + 32 )
+}
 
+function round (numberToRound) {
+	return Math.round( numberToRound * 10 ) / 10;
 }
 
 module.exports = {
